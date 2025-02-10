eb5e183649c36a40d8f431e979b706778cb09911
quangltm1 <quangltm1@gmail.com>
2025-02-10 14:23:00 +0700

    2/10-02 by Quang

--- cakeeflutter/lib/app/page/login/login.dart
+++ cakeeflutter/lib/app/page/login/login.dart
@@ -6,53 +6,99 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
+      backgroundColor: Color(0xFFFFFFFF), // Set background color to white
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
+            Image.asset('assets/images/logo.png'), // Make sure to add your logo in the assets folder
+            SizedBox(height: 20),
+            Text(
+              'Đăng nhập',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                color: Color(0xFFFFD900),
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(2.0, 2.0),
+                    blurRadius: 3.0,
+                    color: Color(0xFF000000),
+                  ),
+                ],
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Tài khoản',
+                  style: TextStyle(fontSize: 16.0, fontFamily: 'Montserrat',),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nhập tài khoản',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Mật khẩu',
+                  style: TextStyle(fontSize: 16.0,
+                  fontFamily: 'Montserrat',
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nhập mật khẩu',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+              ],
             ),
             SizedBox(height: 16.0),
             Row(
               children: <Widget>[
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFD900)),
                   onPressed: () {
-                  // Handle login logic
+                    // Handle login logic
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => HomeScreen()),
                     );
                   },
-                  child: Text('Login'),
+                  child: Text('Đăng nhập', style: TextStyle(color: Colors.black),),
                 ),
                 SizedBox(width: 20.0),
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFD900)),
                   onPressed: () {
-                  // Handle register logic
+                    // Handle register logic
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => RegisterUserScreen()),
                     );
                   },
-                  child: Text('Register'),
+                  child: Text('Đăng ký', style: TextStyle(color: Colors.black),),
                 ),
               ],
             ),
-            
           ],
         ),
       ),
--- cakeeflutter/lib/app/page/login/welcom.dart
+++ cakeeflutter/lib/app/page/login/welcom.dart
@@ -1,21 +1,48 @@
+import 'package:cakeeflutter/app/page/login/login.dart';
 import 'package:flutter/material.dart';
+// Make sure to import the LoginScreen
 
 class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/logo.png'), // Make sure to add your logo in the assets folder
+            children: <Widget>[
+            Image.asset('assets/images/logo.png'), // Make sure to add your logo in the assets folder
             SizedBox(height: 20),
             Text(
               'Cakee',
               style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+              //doi font chu thanh AbeeZee
+              fontFamily: 'AbeeZee',
+              color: Color(0xFFFFD900),
+              //vien chu mau den
+              shadows: <Shadow>[
+                Shadow(
+                  offset: Offset(2.0, 2.0),
+                  blurRadius: 3.0,
+                  color: Color(0xFF000000),
+                ),
+              ],
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFFFFD900)),
+              onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+              },
+              child: Text('Tiếp tục', style: TextStyle(color: Color(0xFF000000))),
             ),
           ],
         ),
